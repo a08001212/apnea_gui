123cc32e770a8d12aecedce23f2a8883e24c238a
= <a08001212@gmail.com>
2024-03-07 15:58:05 +0800

    add state label

--- apnea_gui/apnea_gui/Form1.Designer.cs
+++ apnea_gui/apnea_gui/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace apnea_gui
         {
             this.open_file_btn = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // open_file_btn
@@ -54,17 +55,28 @@ namespace apnea_gui
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.save_to_csv_click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(45, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "請載入影片";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.open_file_btn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-
+            this.PerformLayout();
         }
 
         private System.Windows.Forms.Button open_file_btn;
@@ -72,6 +84,7 @@ namespace apnea_gui
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- apnea_gui/apnea_gui/Form1.cs
+++ apnea_gui/apnea_gui/Form1.cs
@@ -5,14 +5,18 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace apnea_gui
 {
-    
+
     public partial class Form1 : Form
     {
+        private void new_apnea_video_process_thread(){
+            video_process = new ApneaVideoProcess(video_file_path);
+        }
         private string video_file_path;
         private ApneaVideoProcess video_process;
         public Form1()
@@ -25,14 +29,21 @@ namespace apnea_gui
             OpenFileDialog dlg = new OpenFileDialog();
             if (dlg.ShowDialog() != DialogResult.OK) return;
             video_file_path = dlg.FileName;
-            video_process = new ApneaVideoProcess(video_file_path);
+            label1.Text = "分析中...";
+            Thread thread1 = new Thread(new_apnea_video_process_thread);
+            thread1.Start();
+            thread1.Join();
+            label1.Text = "分析結束";
+
         }
 
         private void save_to_csv_click(object sender, EventArgs e)
         {
             OpenFileDialog dlg = new OpenFileDialog();
             if (dlg.ShowDialog() != DialogResult.OK) return;
-            video_process.write_to_csv(dlg.FileName);
+            string save_file_name = dlg.FileName;
+            video_process.write_to_csv(save_file_name);
+            label1.Text = $"儲存至 \"{save_file_name}\" ";
         }
     }
 }
